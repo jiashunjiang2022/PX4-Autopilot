2ec3641248b0f436e611310551bac0a8411be6b9
jiashunjiang2022 <jiashunjiang2022@gmail.com>
2025-10-14 10:19:36 +0800

    修复参数文件格式-移除版权声明头，使用与主分支一致的格式

--- src/modules/fw_lateral_longitudinal_control/fw_lat_long_params.c
+++ src/modules/fw_lateral_longitudinal_control/fw_lat_long_params.c
@@ -1,36 +1,3 @@
-/****************************************************************************
- *
- *   Copyright (c) 2013-2025 PX4 Development Team. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
 /**
  * Path navigation roll slew rate limit.
  *
@@ -60,38 +27,54 @@ PARAM_DEFINE_FLOAT(FW_PN_R_SLEW_MAX, 90.0f);
  */
 PARAM_DEFINE_FLOAT(FW_GND_SPD_MIN, 5.0f);
 
+
+
+
+// ----------longitudinal params----------
+
 /**
- * Height rate feed forward
+ * Throttle max slew rate
+ *
+ * Maximum slew rate for the commanded throttle
  *
  * @min 0.0
  * @max 1.0
  * @decimal 2
- * @increment 0.05
+ * @increment 0.01
  * @group FW Longitudinal Control
  */
-PARAM_DEFINE_FLOAT(FW_T_HRATE_FF, 0.3f);
+PARAM_DEFINE_FLOAT(FW_THR_SLEW_MAX, 0.0f);
 
 /**
- * Altitude error time constant.
+ * Low-height threshold for tighter altitude tracking
  *
- * @min 2.0
- * @decimal 2
- * @increment 0.5
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_ALT_TC, 5.0f);
-
-/**
- * Fast descend: minimum altitude error
+ * Height above ground threshold below which tighter altitude
+ * tracking gets enabled (see FW_LND_THRTC_SC). Below this height, TECS smoothly
+ * (1 sec / sec) transitions the altitude tracking time constant from FW_T_ALT_TC
+ * to FW_LND_THRTC_SC*FW_T_ALT_TC.
  *
- * Minimum altitude error needed to descend with max airspeed and minimal throttle.
- * A negative value disables fast descend.
+ * -1 to disable.
  *
- * @min -1.0
+ * @unit m
+ * @min -1
  * @decimal 0
+ * @increment 1
  * @group FW Longitudinal Control
  */
-PARAM_DEFINE_FLOAT(FW_T_F_ALT_ERR, -1.0f);
+PARAM_DEFINE_FLOAT(FW_T_THR_LOW_HGT, -1.f);
+
+/**
+ * Throttle damping factor
+ *
+ * This is the damping gain for the throttle demand loop.
+ *
+ * @min 0.0
+ * @max 1.0
+ * @decimal 3
+ * @increment 0.01
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_THR_DAMPING, 0.05f);
 
 /**
  * Integrator gain throttle
@@ -122,67 +105,6 @@ PARAM_DEFINE_FLOAT(FW_T_THR_INTEG, 0.02f);
 PARAM_DEFINE_FLOAT(FW_T_I_GAIN_PIT, 0.1f);
 
 /**
- * Pitch damping gain
- *
- * @min 0.0
- * @max 2.0
- * @decimal 2
- * @increment 0.1
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_PTCH_DAMP, 0.1f);
-
-/**
- * Roll -> Throttle feedforward
- *
- * Is used to compensate for the additional drag created by turning.
- * Increase this gain if the aircraft initially loses energy in turns
- * and reduce if the aircraft initially gains energy in turns.
- *
- * @min 0.0
- * @max 20.0
- * @decimal 1
- * @increment 0.5
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_RLL2THR, 15.0f);
-
-/**
- * Maximum descent rate
- *
- * @unit m/s
- * @min 1.0
- * @max 15.0
- * @decimal 1
- * @increment 0.5
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_SINK_MAX, 5.0f);
-
-/**
- * True airspeed error time constant.
- *
- * @min 2.0
- * @decimal 2
- * @increment 0.5
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_TAS_TC, 5.0f);
-
-/**
- * Throttle damping factor
- *
- * This is the damping gain for the throttle demand loop.
- *
- * @min 0.0
- * @max 1.0
- * @decimal 3
- * @increment 0.01
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_THR_DAMPING, 0.05f);
-
-/**
  * Maximum vertical acceleration
  *
  * This is the maximum vertical acceleration
@@ -197,31 +119,6 @@ PARAM_DEFINE_FLOAT(FW_T_THR_DAMPING, 0.05f);
  * @group FW Longitudinal Control
  */
 PARAM_DEFINE_FLOAT(FW_T_VERT_ACC, 7.0f);
-
-/**
- * Specific total energy rate first order filter time constant.
- *
- * This filter is applied to the specific total energy rate used for throttle damping.
- *
- * @min 0.0
- * @max 2
- * @decimal 2
- * @increment 0.01
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_STE_R_TC, 0.4f);
-
-/**
- * Specific total energy balance rate feedforward gain.
- *
- *
- * @min 0.5
- * @max 3
- * @decimal 2
- * @increment 0.01
- * @group FW Longitudinal Control
- */
-PARAM_DEFINE_FLOAT(FW_T_SEB_R_FF, 1.0f);
 
 /**
  * Airspeed measurement standard deviation
@@ -267,35 +164,98 @@ PARAM_DEFINE_FLOAT(FW_T_SPD_DEV_STD, 0.2f);
 PARAM_DEFINE_FLOAT(FW_T_SPD_PRC_STD, 0.2f);
 
 /**
- * Throttle max slew rate
+ * Roll -> Throttle feedforward
  *
- * Maximum slew rate for the commanded throttle
+ * Is used to compensate for the additional drag created by turning.
+ * Increase this gain if the aircraft initially loses energy in turns
+ * and reduce if the aircraft initially gains energy in turns.
+ *
+ * @min 0.0
+ * @max 20.0
+ * @decimal 1
+ * @increment 0.5
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_RLL2THR, 15.0f);
+
+/**
+ * Pitch damping gain
+ *
+ * @min 0.0
+ * @max 2.0
+ * @decimal 2
+ * @increment 0.1
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_PTCH_DAMP, 0.1f);
+
+/**
+ * Altitude error time constant.
+ *
+ * @min 2.0
+ * @decimal 2
+ * @increment 0.5
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_ALT_TC, 5.0f);
+
+/**
+ * Fast descend: minimum altitude error
+ *
+ * Minimum altitude error needed to descend with max airspeed and minimal throttle.
+ * A negative value disables fast descend.
+ *
+ * @min -1.0
+ * @decimal 0
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_F_ALT_ERR, -1.0f);
+
+/**
+ * Height rate feed forward
  *
  * @min 0.0
  * @max 1.0
  * @decimal 2
+ * @increment 0.05
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_HRATE_FF, 0.3f);
+
+/**
+ * True airspeed error time constant.
+ *
+ * @min 2.0
+ * @decimal 2
+ * @increment 0.5
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_TAS_TC, 5.0f);
+
+/**
+ * Specific total energy rate first order filter time constant.
+ *
+ * This filter is applied to the specific total energy rate used for throttle damping.
+ *
+ * @min 0.0
+ * @max 2
+ * @decimal 2
  * @increment 0.01
  * @group FW Longitudinal Control
  */
-PARAM_DEFINE_FLOAT(FW_THR_SLEW_MAX, 0.0f);
+PARAM_DEFINE_FLOAT(FW_T_STE_R_TC, 0.4f);
 
 /**
- * Low-height threshold for tighter altitude tracking
+ * Specific total energy balance rate feedforward gain.
  *
- * Height above ground threshold below which tighter altitude
- * tracking gets enabled (see FW_LND_THRTC_SC). Below this height, TECS smoothly
- * (1 sec / sec) transitions the altitude tracking time constant from FW_T_ALT_TC
- * to FW_LND_THRTC_SC*FW_T_ALT_TC.
  *
- * -1 to disable.
- *
- * @unit m
- * @min -1
- * @decimal 0
- * @increment 1
+ * @min 0.5
+ * @max 3
+ * @decimal 2
+ * @increment 0.01
  * @group FW Longitudinal Control
  */
-PARAM_DEFINE_FLOAT(FW_T_THR_LOW_HGT, -1.f);
+PARAM_DEFINE_FLOAT(FW_T_SEB_R_FF, 1.0f);
 
 /**
  * Wind-based airspeed scaling factor
@@ -310,6 +270,18 @@ PARAM_DEFINE_FLOAT(FW_T_THR_LOW_HGT, -1.f);
  * @group FW Longitudinal Control
  */
 PARAM_DEFINE_FLOAT(FW_WIND_ARSP_SC, 0.f);
+
+/**
+ * Maximum descent rate
+ *
+ * @unit m/s
+ * @min 1.0
+ * @max 15.0
+ * @decimal 1
+ * @increment 0.5
+ * @group FW Longitudinal Control
+ */
+PARAM_DEFINE_FLOAT(FW_T_SINK_MAX, 5.0f);
 
 /**
  * L1 Period
